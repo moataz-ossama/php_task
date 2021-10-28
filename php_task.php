d8f0e00e38ecfaa15bc4874b0046d995958c8d52
moataz_ossama <moataz.ossama.a@gmail.com>
2021-10-28 23:15:27 +0200

    adding php task then push it

---
+++ php_task.php
@@ -0,0 +1,67 @@
+<?php 
+
+$l_1="Free Quote";
+
+$h6_0 ="Digital Media Agency";
+
+$h2_0 ="We Boost Your Website Traffic";
+
+$p1 ="This template is brought to you by TemplateMo website. Feel free to use this for a commercial purpose. You are not allowed to redistribute the template ZIP file on any other template website. Thank you.";
+
+$h6_1="About Us";
+
+$h4_0="Who is DigiMedia <em>Agency</em>";
+
+$p2='We hope this DigiMedia template is useful for your work. You can use this template for any purpose. You may <a rel="nofollow" href="http://paypal.me/templatemo" target="_blank">contribute a little amount</a> via PayPal to <a href="https://templatemo.com/contact" target="_blank">support TemplateMo</a> in creating new templates regularly.';
+
+$h6_2="Our Services";
+
+$h4_1="What Our Agency <em>Provides</em>";
+
+$h4_2="What Our Agency <em>Provides</em>";
+
+$p3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedr do eiusmod deis tempor incididunt ut labore et dolore kengan darwin doerski token.
+                                  dover lipsum lorem and the others.";
+
+$p4="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedr do eiusmod deis tempor incididunt.";
+
+$h4_3= "Healthy Food &amp; Life" ;
+
+
+$h4_4="Car Re-search &amp; Transport";
+
+$h4_5="Online Shopping &amp; Tracking ID";
+
+$h4_6="Enjoy &amp; Travel";
+
+$h6_3="Get Your Free Quote";
+
+$h4_7="Grow With Us Now";
+
+$h6_4="Our Portofolio";
+
+$h4_8="See Our Recent <em>Projects</em>";
+
+$h4_9="Website Builder";
+
+$span_1="Marketing";
+
+$h6_5="Recent News";
+
+$h4_10="Check Our Blog <em>Posts</em>";
+
+$h4_11="Lorem Ipsum Dolor Sit Amet, Consectetur Adelore
+                Eiusmod Tempor Incididunt";
+
+$p5="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doers itii eiumod deis tempor incididunt ut labore.";
+
+$h4_12="Lorem Ipsum Dolor Sit Amei Eiusmod Tempor";
+
+$p12="Lorem ipsum dolor sit amet, cocteturi adipiscing eliterski.";
+
+$h6_6="Contact Us";
+
+$h4_14="Get In Touch With Us <em>Now</em>";
+
+ ?>
+
